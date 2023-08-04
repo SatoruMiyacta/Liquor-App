db2b7f4910c26603120cbad806463f9098b84d1c
Miyacta <s.miya211@gmail.com>
2023-08-04 22:28:54 +0900

    main修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { initializeApp } from "firebase/app";
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -16,6 +16,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
 
 const router = createBrowserRouter([
   {
